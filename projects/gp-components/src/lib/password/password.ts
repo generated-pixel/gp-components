fb795d54badc13bf8dff8388f5acf3642f6208e7
Graeme <graeme.gorman@hays.com>
2025-11-17 13:11:15 -0500

    Remove tslib

---
+++ projects/gp-components/src/lib/password/password.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gp-password',
+  imports: [],
+  templateUrl: './password.html',
+  styleUrl: './password.css',
+})
+export class Password {
+
+}
